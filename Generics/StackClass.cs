0aef2c745da6e3b2d8d0975919ab1148ef628ca5
mykelgozie <mykelgozie@gmail.com>
2020-08-02 22:26:08 +0100

    just created stack generics

---
+++ Generics/StackClass.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Generics
+{
+    class StackClass<T>
+    {
+
+        private List<T> items;
+
+        //creates a new Constructor list
+        public StackClass()
+        {
+
+            items = new List<T>();
+        }
+
+        //Get item from list
+        public T GetIndex(int index)
+        {
+            return items[index];
+        }
+
+        //insert item at the end of list
+        public void Push(T newitem)
+        {
+            items.Add(newitem);
+
+        }
+
+        //Get item size
+        public int Size()
+        {
+
+            return items.Count;
+        }
+
+         //Get item at the end of list
+        public T Peek()
+        {
+            int last = items.Count - 1;
+            return items[last];
+        }
+
+        //checked if empty
+        public bool IsEmpty()
+        {
+            if (items.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //remove element at the end
+        public T Pop()
+        {
+
+            int last = items.Count;
+            T element = items[last - 1];
+            items.RemoveAt(last - 1);
+            return element;
+        }
+
+        //get all elements
+        public void Print()
+        {
+            if (items.Count > 0)
+            {
+
+                for (int i = items.Count - 1; i >= 0; i--)
+                {
+
+                    Console.WriteLine(items[i]);
+                }
+
+
+            }
+
+        }
+
+
+
+
+
+    }
+}
